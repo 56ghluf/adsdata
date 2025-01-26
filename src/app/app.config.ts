2fe10dbf0546582d7ba1d4d1607efcdd9cf452da
famemonkey <redbirdhasflown@gmail.com>
2025-01-26 11:00:16 +0100

    no idea what i am doing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "adsdata-17f84", appId: "1:573712589308:web:22859226831fcc2dde1279", storageBucket: "adsdata-17f84.firebasestorage.app", apiKey: "AIzaSyDL2Z0nycJ6EgmpK5dygdO7TwJj2FAyYpI", authDomain: "adsdata-17f84.firebaseapp.com", messagingSenderId: "573712589308" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDL2Z0nycJ6EgmpK5dygdO7TwJj2FAyYpI",
+        authDomain: "adsdata-17f84.firebaseapp.com",
+        projectId: "adsdata-17f84",
+        storageBucket: "adsdata-17f84.firebasestorage.app",
+        messagingSenderId: "573712589308",
+        appId: "1:573712589308:web:819ae40909efa8d2de1279"
+    }
+}
