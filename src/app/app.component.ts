6ba4358df6506b5d0e6655d97eec7c1cd8d06ea7
famemonkey <redbirdhasflown@gmail.com>
2025-01-26 14:23:11 +0100

    got reading and writing to firebase database working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Firestore, collection, collectionData, addDoc, CollectionReference, DocumentReference } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'ads-data';
+  firestore: Firestore = inject(Firestore);
+  statements: Observable<any[]>;
+  pvnCollection: CollectionReference;
+
+  constructor() {
+    this.pvnCollection = collection(this.firestore, 'PosVsNorm');
+    this.statements = collectionData(this.pvnCollection);
+  }
+
+  onSubmit(event: any) {
+    event.preventDefault()
+    addDoc(this.pvnCollection, <Statement> { sentence: event.target.query.value }).then(
+      (documentReference: DocumentReference) => {
+        console.log(documentReference);
+      }
+    );
+    event.target.query.value = "";
+  }
+}
+
+export interface Statement {
+  sentence: string;
 }
